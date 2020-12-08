e2946534de09a52e0e897d295c10fd19648565fd
Joakim SI ALI <joakim.siali@gmail.com>
2020-12-08 12:13:40 +0100

    gitignore + 2015

---
+++ 2015/Controllers/Day1.cs
@@ -0,0 +1,26 @@
+using System.IO;
+using System;
+
+namespace Controllers
+{
+    public class Day1
+    {
+        public Day1() {
+
+        }
+
+        public static void Run() {
+            Console.WriteLine(Part1());
+            Console.WriteLine(Part2());
+        }
+
+        public static void Part1() {
+            Console.Write(File.ReadAllText("./inputs/day1.txt"));
+
+        }
+
+        public static void Part2() {
+            
+        }
+    }
+}
---
+++ 2015/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Controllers;
+
+namespace _2015
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            switch(args[0]) {
+                case "day1": {
+                    Day1.Run();
+                    break;
+                }
+            }
+        }
+    }
+}
