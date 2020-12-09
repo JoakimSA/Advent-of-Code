625059cd88a850efe3f14099fc77ca5cf7ac9f77
Joakim SI ALI <joakim.siali@gmail.com>
2020-12-09 12:03:15 +0100

    2015 - Day2

--- 2015/Program.cs
+++ 2015/Program.cs
@@ -7,11 +7,23 @@ namespace _2015
     {
         public static void Main(string[] args)
         {
-            switch(args[0]) {
-                case "day1": {
-                    Day1.Run();
-                    break;
+            if(args.Length > 0) {
+                /*Type t = Type.GetType(args[0]);
+
+                Console.WriteLine(t);
+
+                var c = Activator.CreateInstance(t);*/
+
+                switch(args[0]) {
+                    case "day1":
+                        Day1.Run();
+                        break;                
+                    case "day2":
+                        Day2.Run();
+                        break;                
                 }
+            } else {
+                Console.WriteLine("Missing arguments");
             }
         }
     }
